960bca7d95635a291e5621e851b514d0df3d216c
Victor Vitalino <victorvitalino237@gmail.com>
2021-02-27 22:58:12 -0300

    web chat project-first commit

---
+++ server/src/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const server = express();
+
+server.listen(3000, () => console.log("server is runing"));
---
+++ web/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Web chat</h1>;
+}
+
+export default App;
